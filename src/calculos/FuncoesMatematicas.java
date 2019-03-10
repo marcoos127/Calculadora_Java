e1adb94353651359f28757f1606f0ba8c6ef507d
marcoos127 <mlkzika60@live.com>
2019-03-10 12:37:58 -0300

    Primeiro Commit

---
+++ src/calculos/FuncoesMatematicas.java
@@ -0,0 +1,24 @@
+package calculos;
+
+public class FuncoesMatematicas {
+	public double soma(double a, double b) {
+		double total = a + b;
+		return total;
+	}
+	public double subt(double c, double d) {
+		double totsub = c - d;
+		return totsub;
+	}
+	public double mult(double e, double f) {
+		double totmult = e * f;
+		return totmult;
+	}
+	public double div(double g, double h) {
+		double totdiv = g / h;
+		return totdiv;
+	}
+	public double mod(double i, double j) {
+		double totmod = i % j;
+		return totmod;
+	}
+}
